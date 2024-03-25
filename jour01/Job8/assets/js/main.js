35d7f1faa9a94cb4b8c3fc57e572826f6f7e6075
lsDevPlateforme <163527712+lsDevPlateforme@users.noreply.github.com>
2024-03-25 13:35:32 +0100

    corrrection isPrime

--- jour01/Job8/assets/js/main.js
+++ jour01/Job8/assets/js/main.js
@@ -1,4 +1,13 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
 const sommeNombresPremiers = (nb1, nb2) => {
-  if (nb1 % 2 === 0 && nb2 % 2 === 0) return nb1 + nb2;
+  if (isPrime(nb1) && isPrime(nb2)) return nb1 + nb2;
   else return false;
 };
