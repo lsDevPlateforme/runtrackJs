d321e7e60003989c2faaacf2a53c0bb0c10597d7
Liron Saiman <163527712+lsDevPlateforme@users.noreply.github.com>
2024-04-04 17:41:22 +0200

    Add files via upload

---
+++ Jour08/Job05/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/header/Header";
+import { Home } from "./components/home/Home";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Home />
+      </main>
+    </>
+  );
+};
---
+++ Jour08/Job05/src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+export const Header = () => {
+  return (
+    <header className=" w-screen bg-gray-img sticky top-0 backdrop-blur-md bg-opacity-80 z-20">
+      <nav className="py-4 flex items-center justify-center gap-4 text-sm font-bold uppercase">
+        <a
+          href=""
+          className="py-3 px-6 transition-property:background-color duration-200  rounded-full hover:bg-black/10"
+        >
+          Speakers
+        </a>
+        <a
+          href=""
+          className="py-3 px-6 transition-property:background-color duration-200  rounded-full hover:bg-black/10"
+        >
+          Schedule
+        </a>
+        <a
+          href=""
+          className="py-3 px-6 transition-property:background-color duration-200  rounded-full hover:bg-black/10"
+        >
+          Partners
+        </a>
+        <a
+          href=""
+          className="py-3 px-6 transition-property:background-color duration-200  rounded-full hover:bg-black/10"
+        >
+          Mingle
+        </a>
+        <a
+          href=""
+          className="py-3 px-6 transition-property:background-color duration-200  rounded-full hover:bg-black/10"
+        >
+          Pre-Party
+        </a>
+        <a
+          href=""
+          className="py-3 px-6 transition-property:background-color duration-200  rounded-full hover:bg-black/10"
+        >
+          Swag
+        </a>
+        <a
+          href=""
+          className="py-3 px-6 transition-property:background-color duration-200  rounded-full text-white bg-black hover:bg-slate-800"
+        >
+          Past videos
+        </a>
+      </nav>
+    </header>
+  );
+};
